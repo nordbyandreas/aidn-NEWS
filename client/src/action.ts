4a303576ee8446f6f99825ae50d1670b03993938
nordbyandreas <nordby.an@gmail.com>
2025-03-19 09:33:12 +0100

    comment out bug

--- client/src/action.ts
+++ client/src/action.ts
@@ -49,7 +49,7 @@ export default async function validateAndSubmitMeasurements(
         },
         {
           type: "rr",
-          value: Number(formData.get("rr")) + 999,
+          value: Number(formData.get("rr")), // + 999,
         },
       ],
     }),
